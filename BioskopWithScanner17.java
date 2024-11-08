f35eef06b538b02a4d435aa0b93a965058bf82cf
daffifiros <daffikun10@gmail.com>
2024-11-08 14:18:32 +0700

    Hasil Percobaan 3

--- BioskopWithScanner17.java
+++ BioskopWithScanner17.java
@@ -5,20 +5,68 @@ public class BioskopWithScanner17 {
         Scanner scanner = new Scanner(System.in);
         String[][] penonton = new String[4][2];
 
-        while (true) {
-            System.out.print("Masukkan nama: ");
-            String nama = scanner.nextLine();
-            System.out.print("Masukkan baris: ");
-            int baris = scanner.nextInt();
-            System.out.print("Masukkan kolom: ");
-            int kolom = scanner.nextInt();
+//         while (true) {
+//             System.out.print("Masukkan nama: ");
+//             String nama = scanner.nextLine();
+//             System.out.print("Masukkan baris: ");
+//             int baris = scanner.nextInt();
+//             System.out.print("Masukkan kolom: ");
+//             int kolom = scanner.nextInt();
+//             scanner.nextLine(); 
+//             penonton[baris - 1][kolom - 1] = nama;
+//             System.out.print("Input penonton lainnya? (y/n): ");
+//             String lanjut = scanner.nextLine();
+//             if (lanjut.equalsIgnoreCase("n")) {
+//                 break;
+//             }
+//         }
+//     }
+// }
+        // Modifikasi memberikan opsi menu
+        int pilihan;
+        do {
+            System.out.println("\nMenu:");
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            System.out.print("Pilih menu: ");
+            pilihan = scanner.nextInt();
             scanner.nextLine(); 
-            penonton[baris - 1][kolom - 1] = nama;
-            System.out.print("Input penonton lainnya? (y/n): ");
-            String lanjut = scanner.nextLine();
-            if (lanjut.equalsIgnoreCase("n")) {
-                break;
+
+            switch (pilihan) {
+                case 1:
+        
+                    while (true) {
+                        System.out.print("Masukkan nama: ");
+                        String nama = scanner.nextLine();
+                        System.out.print("Masukkan baris: ");
+                        int baris = scanner.nextInt();
+                        System.out.print("Masukkan kolom: ");
+                        int kolom = scanner.nextInt();
+                        scanner.nextLine();
+                        penonton[baris - 1][kolom - 1] = nama;
+                        System.out.print("Input penonton lainnya? (y/n): ");
+                        String lanjut = scanner.nextLine();
+                        if (lanjut.equalsIgnoreCase("n")) {
+                            break;
+                        }
+                    }
+                    break;
+                case 2:
+                    System.out.println("\nDaftar Penonton:");
+                    for (int i = 0; i < penonton.length; i++) {
+                        for (int j = 0; j < penonton[i].length; j++) {
+                            System.out.print(penonton[i][j] + "\t");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 3:
+                    System.out.println("Terima kasih!");
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid.");
             }
-        }
+        } while (pilihan != 3);
     }
 }
