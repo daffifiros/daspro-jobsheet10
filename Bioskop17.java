90452ffd9a506c6a52bc26bd6b47ac1b3a5d1cfc
daffifiros <daffikun10@gmail.com>
2024-11-08 08:36:39 +0700

    Hasil Modifikasi

--- Bioskop17.java
+++ Bioskop17.java
@@ -11,9 +11,40 @@ public class Bioskop17 {
         penonton[2][0] = "Eka";
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
+        //tambahan
+        penonton[3][1] = "Hana";
         System.out.printf("%s \t %s \n", penonton[0][0], penonton[0][1]);
         System.out.printf("%s \t %s \n", penonton[1][0], penonton[1][1]);
         System.out.printf("%s \t %s \n", penonton[2][0], penonton[2][1]);
-        System.err.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
+        System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
+        //tambahan kode program
+        System.out.println(penonton.length);
+        System.out.println(penonton[0].length);
+        System.out.println(penonton[1].length);
+        System.out.println(penonton[2].length);
+        System.out.println(penonton[3].length);
+        // tambahan for loop 1
+        System.out.println(penonton.length);
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Panjang baris ke-" + (i + 1) + ": " + penonton[i].length);
+        }
+        //tambahan foreach loop
+        System.out.println(penonton.length);
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris: " + barisPenonton.length);
+        }
+        // tambahan for loop 2
+        System.out.println("Penonton pada baris ke-3: ");
+        for (int i = 0; 1 < penonton[2].length; i++) {
+            System.out.println(penonton[2][i]);
+        }
+        // tambahan foreach loop 2
+        System.out.println("Penonton pada baris ke-3: ");
+        for (String i : penonton[2]) {
+            System.out.println(i);
+        }
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton baris ke- " + (i+1) + ": " + String.join(", ", penonton[i] ));
+        }
     }
 }
